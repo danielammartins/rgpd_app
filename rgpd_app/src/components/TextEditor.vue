25463e1998eda594c2303b778c8c9bea7ff62d92
danielammartins <martins.danielamadeira@gmail.com>
2022-06-24 21:10:01 +0100

    can get and set text in editor

--- rgpd_app/src/components/TextEditor.vue
+++ rgpd_app/src/components/TextEditor.vue
@@ -1,57 +1,75 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import { getText, updateText } from '../services/EditorServices'
 
-let dateTime = null;
+const editor = ref(null)
+const quill = ref(null)
+const dateTime = ref('')
+const content = ref({})
+const options = reactive({
+  modules: {
+    syntax: false,
+    toolbar: '#toolbar-container'
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'
+})
 
-const content = ref({});
+onMounted(() => {
+  if(editor.value !== null) {
+    quill.value = new Quill(editor.value, options)
+  }
+})
 
 const getContent = async () => {
-  /*
-  const response = await fetch('http://localhost:3011/api/text');
-  const json = await response.json();
-
-  content.value = JSON.parse(json);
-
-  console.log("Resposta" + content.value);*/
-  const data = await fetch('http://localhost:3011/api/text').then(res => res.json());
-
-  console.log(data);
+  const data = await (await fetch('http://localhost:3011/tecontent/1')).json();
+  if(data.length >= 1) {
+    content.value = JSON.parse(data[0].content)
+    if(quill.value !== null) {
+      quill.value.setContents(content.value)
+    }
+    console.log(content.value);
+  }
 };
+onMounted(getContent);
 
-
-//onMounted(getContent);
-/*
 const setContent = async () => {
-  await fetch('http://localhost:3011/delta/delta', {
+  if(quill.value !== null) {
+    //content.value = quill.value.getContents()
+  }
+  await fetch('http://localhost:3011/tecontent/1', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ title: 'delta', content: JSON.stringify(content.value)})
+    body: JSON.stringify({ 
+      content: JSON.stringify(content.value)
+    })
   });
 
   await getContent();
  
   alert('Conteúdo atualizado com sucesso!');
-};*/
+};
 
 const updateTime =  () => {
   let current = new Date();
   let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
   let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
   
-  dateTime = cDate + ' ' + cTime;
+  dateTime.value = cDate + ' ' + cTime;
 
-  console.log("Updated time: " + dateTime);
+  console.log("Updated time: " + dateTime.value);
 }
 
 </script>
 
 <template>
-  <QuillEditor v-model:content="content" toolbar="#custom-toolbar">
+  <QuillEditor v-model:content="content" contentType="delta" toolbar="#custom-toolbar">
     <template #toolbar>
       <div id="custom-toolbar">
         <select class="ql-header">
@@ -75,10 +93,59 @@ const updateTime =  () => {
         <button class="ql-list" value="ordered"></button>
         <button class="ql-list" value="bullet"></button>
         <button class="ql-link"></button>
-        <button type="button" id="your-button" @click="() => {getContent(); updateTime()}">Save</button>
+        <button type="button" id="your-button" @click="() => {setContent(); updateTime()}">Save</button>
       </div>
     </template>
   </QuillEditor>
+
+  <div id="toolbar-container">
+    <span class="ql-formats">
+      <select class="ql-font"></select>
+      <select class="ql-size"></select>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-bold"></button>
+      <button class="ql-italic"></button>
+      <button class="ql-underline"></button>
+      <button class="ql-strike"></button>
+    </span>
+    <span class="ql-formats">
+      <select class="ql-color"></select>
+      <select class="ql-background"></select>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-script" value="sub"></button>
+      <button class="ql-script" value="super"></button>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-header" value="1"></button>
+      <button class="ql-header" value="2"></button>
+      <button class="ql-blockquote"></button>
+      <button class="ql-code-block"></button>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-list" value="ordered"></button>
+      <button class="ql-list" value="bullet"></button>
+      <button class="ql-indent" value="-1"></button>
+      <button class="ql-indent" value="+1"></button>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-direction" value="rtl"></button>
+      <select class="ql-align"></select>
+    </span>
+    <span class="ql-formats">
+      <button class="ql-link"></button>
+      <button class="ql-image"></button>
+      <button class="ql-video"></button>
+      <button class="ql-formula"></button>
+    </span>
+    <span class="ql-formats">
+      <!--<button class="ql-clean"></button>-->
+      <button id="custom-button" @click="setContent(); updateTime();">Save</button>
+    </span>
+  </div>
+  
+  <div ref="editor"></div>
 
   <!-- Tem de ir buscar a data do último update -->
   <p id="lastUpdate">Última atualização: {{ dateTime }}</p>

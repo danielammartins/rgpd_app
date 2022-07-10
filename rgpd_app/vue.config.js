ece311b120e3ab37d6e7e2167140be5b80de03ed
filipasimoe <filipa.barreiro16@gmail.com>
2022-07-10 02:20:42 +0100

    fixed https

--- rgpd_app/vue.config.js
+++ rgpd_app/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3011',
+        target: 'https://localhost:3011',
         changeOrigin: true
       },
     }
